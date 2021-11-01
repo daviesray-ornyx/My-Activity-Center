e50386c2058bf32be33a4c37cd88a171927b9f61
daviesray-ornyx <daviesray.ornyx@gmail.com>
2021-11-01 12:07:02 +0300

    Adding file pteview -intermediate

--- MyActivityCenter/lib/LocalFile.cs
+++ MyActivityCenter/lib/LocalFile.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,5 +14,28 @@ namespace MyActivityCenter.lib
         public string NameWithExtension { get; set; }
         public string DirectoryPath { get; set; }
         public string FullPath { get; set; }
+        private string _previewPath;
+        public string PreviewPath {
+            get
+            {
+                if (String.IsNullOrEmpty(_previewPath))
+                {
+                    // try getting the path again
+                    Directory.CreateDirectory(Path.Combine(DirectoryPath, @"preview-cache")); // create directory if it does not exist
+
+                    // Try setting the _previewPath
+                    _previewPath =
+                }
+
+                _previewPath = Path.Combine(DirectoryPath, @"preview-cache", NameWithExtension);
+                return _previewPath;
+            }
+            set
+            {
+                if (value == _previewPath)
+                    return;
+                _previewPath = value;
+            }
+        }
     }
 }
